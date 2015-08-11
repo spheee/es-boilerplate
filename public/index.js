70fd172eb054b5875cfa051773e27fa3368a95b6
kshy <habxp@163.com>
2015-08-11 14:39:22 +0800

    ff fix

---
+++ public/index.js
@@ -0,0 +1,22 @@
+$('.dropdown-toggle').on('click', function() {
+	//取消默认
+	event.preventDefault();
+
+	event.stopPropagation()
+
+	var self = $(this),
+		href = self.attr('href');
+	if (href) {
+
+		window.location.href = href;
+		return;
+	}
+
+}).on('mouseenter', function() {
+
+	//版本太高了 hover不能用了
+	$(this).dropdown('toggle')
+
+}).next('ul').on('mouseleave', function() {
+	// $(this).dropdown('toggle')
+});
